3f501937d7f3c014cece823583e44d770c6950fb
Rinaldi <Emily_Rinaldi@tsged.com>
2016-08-24 21:56:42 -0500

    First model attempts

--- data_wrangling.R
+++ data_wrangling.R
@@ -16,10 +16,8 @@ census <- census %>% select(geo = X3,
                             tot_pop = X4, 
                             med_age = X42, 
                             pct_over65 = X53, 
-                            pct_white = X159,
                             pct_black = X161,
                             pct_hisp = X231,
-                            pct_wh_hisp = X233,
                             pct_nonhisp_wh = X249,
                             pct_married_houses = X309,
                             pct_single_moth_houses = X319) %>%
@@ -43,7 +41,7 @@ facilities <- facilities %>%
   mutate()
 
 #Read in ACS economic survey data
-econ_surv <- read_csv(econ_surv_file, skip = 2, col_names = FALSE)
+econ_surv <- read_csv(econ_surv_file, skip = 2, col_names = FALSE, na = c("", NA, "(X)", "-", "N"))
 econ_surv <- econ_surv %>% select(geo = X3,
                                   pct_labor_force = X10,
                                   pct_unemployed = X38,
@@ -61,10 +59,13 @@ econ_surv <- econ_surv %>% select(geo = X3,
   mutate(geo = tolower(geo))
 econ_surv <-  mutate(econ_surv, geo = sub(" county, california$", "", x = econ_surv$geo))
 econ_surv <-  mutate(econ_surv, geo = sub("^zcta5 ", "", x = econ_surv$geo))
+econ_surv$median_worker_earnings <- as.numeric(econ_surv$median_worker_earnings)
 
 #Join datasets
 all_data <- left_join(facilities, left_join(econ_surv, census), by = c("zip" = "geo"))
-all_data$per_cap_income <- as.numeric(all_data$per_cap_income)
+all_data <- separate(all_data, ownership, c("owner", "owner_type"), sep = " - ")
+all_data$MSSA_desig <- factor(all_data$MSSA_desig)
+all_data$owner <- factor(all_data$owner)
 
 
 #Make plots
@@ -72,3 +73,47 @@ pct_table <- select(all_data, year:location, starts_with("pct_"))
 pct_table_long <- gather(pct_table, "stat", "value", pct_labor_force:pct_single_moth_houses)
 ggplot(pct_table_long, aes(x = value, y = pct_comm, col = stat, fill = stat)) + geom_point(alpha = 0.4) + geom_smooth(se = FALSE, size = 2, method = lm) + xlab("value (as % of population)") + ylab("percent commercial patients") + facet_grid(. ~ owner)
 ggplot(all_data, aes(x = per_cap_income, y = pct_comm)) + geom_point(alpha = 0.3) + geom_smooth(method = lm)
+
+
+## caret modeling attempts
+#prepare data for training
+
+model_data <- all_data %>% ungroup() %>% select(-(year:tot_volume))
+model_data <- na.omit(model_data)
+
+inTraining <- createDataPartition(model_data$pct_comm, p = .8, list = FALSE)
+training <- model_data[inTraining,]
+testing <- model_data[-inTraining,]
+fitControl <- trainControl(method = "repeatedcv",
+                           number = 10,
+                           repeats = 10)
+## set.seed??
+Fit1 <- train(pct_comm ~ ., data = model_data,
+              method = "lm",
+              trControl = fitControl,
+              preProcess = c("pca"))
+
+Fit2 <- train(pct_comm ~., data = model_data, method = "lm")
+
+##attempting to remove variables to improve model
+fit3 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, - pct_SNAP)), method = "lm")
+summary(fit3)
+fit4 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings)), method = "lm")
+summary(fit4)
+varImp(fit4)
+fit5 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65)), method = "lm")
+summary(fit5)
+fit5 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp)), method = "lm")
+summary(fit5)
+fit6 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_private_ins)), method = "lm")
+summary(fit6)
+fit6 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_public_ins, -pct_no_ins)), method = "lm")
+summary(fit6)
+fit7 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_public_ins, -pct_no_ins, -pct_unemployed)), method = "lm")
+summary(fit7)
+fit8 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_public_ins, -pct_no_ins, -pct_unemployed, - pct_poverty)), method = "lm")
+summary(fit8)
+fit9 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_public_ins, -pct_no_ins, -pct_unemployed, - pct_poverty, - pct_black, -pct_hisp)), method = "lm")
+summary(fit9)
+fit10 <- train(pct_comm ~ ., data = na.omit(select(model_data, -pct_single_moth_houses, -pct_SNAP, -median_household_income, -mean_household_income, -mean_fam_income, -per_cap_income, -median_worker_earnings, -pct_over65, -pct_wh_hisp, -pct_public_ins, -pct_no_ins, -pct_unemployed, - pct_poverty, - pct_black, -pct_hisp, -MSSA_desig)), method = "lm")
+summary(fit10)
